a53c74d18f055800aedaf21d554b99dbe84451c6
hojie11 <hjkim199596@gmail.com>
2020-12-13 04:53:41 +0900

    edited

--- Create_Sphere.py
+++ Create_Sphere.py
@@ -6,18 +6,19 @@ import pybullet_data
 # Make simulation world
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.createCollisionShape(p.GEOM_PLANE)
+
+plainID = p.loadURDF('plane.urdf')
 
 # Set gravity to z-axis
 p.setGravity(0, 0, -9.8)
 
-plainID = p.loadURDF('plane.urdf')
 
 # Sphere parametre
 mass = 1
 sphereR = 0.3
 visualSphereID = -1
 
+p.createCollisionShape(p.GEOM_PLANE)
 SphereID = p.createCollisionShape(p.GEOM_SPHERE, radius=sphereR)
 sphereStartPos = [0, 0, 0.5]
 SphereStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
