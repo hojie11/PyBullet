7524961d76d98ca07b85af2a4fff33995dc4e9aa
hojie11 <hjkim199596@gmail.com>
2020-12-13 04:31:53 +0900

    comment

---
+++ Create_Sphere.py
@@ -0,0 +1,38 @@
+# Example : create a ball by pressing a button
+import pybullet as p
+import time
+import pybullet_data
+
+# Make simulation world
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.createCollisionShape(p.GEOM_PLANE)
+
+# Set gravity to z-axis
+p.setGravity(0, 0, -9.8)
+
+plainID = p.loadURDF('plane.urdf')
+
+# Sphere parametre
+mass = 1
+sphereR = 0.3
+visualSphereID = -1
+
+SphereID = p.createCollisionShape(p.GEOM_SPHERE, radius=sphereR)
+sphereStartPos = [0, 0, 0.5]
+SphereStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+
+while True:
+    p.stepSimulation()
+
+    # Press 'c' to create a ball
+    # 'c' = create
+    keys = p.getKeyboardEvents()
+    for k, v in keys.items():
+        #print(k, v)
+        if (k == 99 and v & p.KEY_WAS_RELEASED):
+            SphereUID = p.createMultiBody(mass, SphereID, 1, sphereStartPos, SphereStartOrientation)
+
+    time.sleep(1/200)
+
+p.disconnect()
--- PyBullet_test.py
+++ PyBullet_test.py
@@ -7,12 +7,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8) # set gravity vector to z-axis
 p.setTimeStep(1/200) # update per 200 frames
 
-planeID = p.loadURDF("plane.urdf") # load .urdf file
+planeID = p.loadURDF("samurai.urdf") # load .urdf file
 cubeStartPos = [2, -1, 1] # set position of object
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 3.14]) # set rotation of object 
                                                                                                 # should convert euler to Quaternion
 robotID = p.loadURDF("r2d2.urdf", cubeStartPos, cubeStartOrientation) # load robot .urdf file by using parameters you set
-
+    
 for i in range(10000):
     p.stepSimulation()
     time.sleep(1/200)
